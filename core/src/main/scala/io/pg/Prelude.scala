ac8f1f335b6a4e8282bf65a43beaa6ab529a84a9
Jakub Kozłowski <kubukoz@gmail.com>
2020-07-25 04:41:12 +0200

    private

--- core/src/main/scala/io/pg/Prelude.scala
+++ core/src/main/scala/io/pg/Prelude.scala
@@ -5,7 +5,7 @@ import cats.syntax.functor._
 
 object Prelude {
 
-  implicit class EffectToResourceLiftSyntax[F[_], A](val fa: F[A]) extends AnyVal {
+  implicit class EffectToResourceLiftSyntax[F[_], A](private val fa: F[A]) extends AnyVal {
 
     def resource(implicit F: Applicative[F]): Resource[F, A] = Resource.liftF(fa)
 
