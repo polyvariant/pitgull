e0c587bd46024138ca45ce5f5976d2901e7c3609
Jakub Kozłowski <kubukoz@gmail.com>
2020-08-14 02:47:48 +0200

    Revert "Remove all irrelevant sources"
    This reverts commit eb6c3d93d987d3dfb2fd28d7d752e543adf98be1.

--- src/test/scala/io/pg/MainTest.scala
+++ src/test/scala/io/pg/MainTest.scala
@@ -1,9 +1,22 @@
 package io.pg
 
 import org.scalatest.wordspec.AsyncWordSpec
+import cats.effect.IO
+import cats.implicits._
+import ciris.Secret
 import io.pg.gitlab.transport.WebhookEvent
 
 class MainTest extends AsyncWordSpec {
   println(WebhookEvent.codec)
+  "Application" should {
+    "start" in {
+      val testConfig = AppConfig(
+        HttpConfig(8080),
+        MetaConfig("-", BuildInfo.version, BuildInfo.scalaVersion),
+        Git(Git.Host.Gitlab, "http://localhost", Secret("token"))
+      )
 
+      Main.serve(testConfig).use(IO.pure).as(succeed).unsafeToFuture()
+    }
+  }
 }
