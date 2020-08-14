21f4dd0daf14c4988e06e6d2bd6364cff31f6edc
Jakub Kozłowski <kubukoz@gmail.com>
2020-08-14 02:27:59 +0200

    Revert debug utils

--- gitlab/src/main/scala/io/pg/gitlab/transport/transport.scala
+++ gitlab/src/main/scala/io/pg/gitlab/transport/transport.scala
@@ -3,12 +3,11 @@ package io.pg.gitlab.transport
 import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.semiauto._
 import io.circe.Codec
-import com.kubukoz.DebugUtils
 
 object CirceConfiguration {
 
   implicit val config: Configuration =
-    DebugUtils.withDesugar(Configuration.default.withSnakeCaseMemberNames.withSnakeCaseConstructorNames)
+    Configuration.default.withSnakeCaseMemberNames.withSnakeCaseConstructorNames
 }
 
 import CirceConfiguration._
@@ -23,10 +22,10 @@ object WebhookEvent {
     case object Push extends ObjectKind
     case object MergeRequest extends ObjectKind
 
-    implicit val codec: Codec[ObjectKind] = DebugUtils.withDesugar(deriveEnumerationCodec)
+    implicit val codec: Codec[ObjectKind] = deriveEnumerationCodec
   }
 
-  implicit val codec: Codec[WebhookEvent] = DebugUtils.withDesugar(deriveConfiguredCodec)
+  implicit val codec: Codec[WebhookEvent] = deriveConfiguredCodec
 }
 
 final case class Project(
@@ -38,5 +37,5 @@ final case class Project(
 )
 
 object Project {
-  implicit val codec: Codec[Project] = DebugUtils.withDesugar(deriveConfiguredCodec)
+  implicit val codec: Codec[Project] = deriveConfiguredCodec
 }
