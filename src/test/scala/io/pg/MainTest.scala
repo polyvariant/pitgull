8275088f33242b9c4ad3a37e9650980d8d799eb1
Jakub Kozłowski <kubukoz@gmail.com>
2020-08-14 02:47:47 +0200

    Revert "foo"
    This reverts commit 36fa516f0e8d39945dbff5a7fdf415087f835427.

--- src/test/scala/io/pg/MainTest.scala
+++ src/test/scala/io/pg/MainTest.scala
@@ -2,13 +2,8 @@ package io.pg
 
 import org.scalatest.wordspec.AsyncWordSpec
 import io.pg.gitlab.transport.WebhookEvent
-import scala.util.Try
 
 class MainTest extends AsyncWordSpec {
-  def tp(s: => Any) = Try(println(s))
-  tp(getClass.getClassLoader().loadClass("io.circe.Decoder"))
-  tp(getClass.getClassLoader().loadClass("io.circe.generic.extras.semiauto"))
-  tp(getClass.getClassLoader().loadClass("io.circe.generic.extras.Configuration"))
   println(WebhookEvent.codec)
 
 }
