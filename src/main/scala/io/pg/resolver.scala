baf30eb5df55b8ef9caeae4a7b689c076153d8b1
Jakub Kozłowski <kubukoz@gmail.com>
2020-09-06 02:32:36 +0200

    Abolish OptionT

--- src/main/scala/io/pg/resolver.scala
+++ src/main/scala/io/pg/resolver.scala
@@ -1,6 +1,5 @@
 package io.pg
 
-import cats.data.OptionT
 import cats.tagless.finalAlg
 import io.pg.gitlab.webhook.WebhookEvent
 import io.pg.gitlab.Gitlab
@@ -13,6 +12,8 @@ import io.pg.gitlab.graphql.Pipeline
 import io.pg.gitlab.graphql.User
 import io.pg.gitlab.Gitlab.GitlabError
 import io.pg.gitlab.webhook.Project
+import scala.util.chaining._
+import cats.Applicative
 
 @finalAlg
 trait StateResolver[F[_]] {
@@ -24,35 +25,48 @@ object StateResolver {
   //Option - some events don't yield a state to work with and should be ignored.
   //We should get an ADT for this.
   def instance[F[_]: Gitlab: Logger: MonadError[*[_], Throwable]]: StateResolver[F] =
+    // Implementation note: all effectful methods here can fail with Halt,
+    // which should be handled gracefully as a reason for an incomplete state.
     new StateResolver[F] {
+      private case class Halt(msg: String) extends Throwable
 
-      private def decodeMergeRequest(pipeline: WebhookEvent.Pipeline): OptionT[F, io.pg.gitlab.webhook.MergeRequest] = {
-        val project = pipeline.project
+      private def halt[A](msg: String): F[A] = Halt(msg).raiseError[F, A]
 
-        val mergeRequestByHeadPipeline = {
-          val mergeRequestIdString: OptionT[F, String] =
-            OptionT {
-              Gitlab[F]
-                .mergeRequests(
-                  projectPath = project.pathWithNamespace,
-                  sourceBranches = NonEmptyList.of(pipeline.objectAttributes.ref)
-                )(
-                  MergeRequest.iid ~ MergeRequest.headPipeline(Pipeline.id)
-                )
-                .map(_.headOption)
-            }.collect {
-              case (mrIid, Some(s"gid://gitlab/Ci::Pipeline/$headPipelineId"))
-                  if headPipelineId === pipeline.objectAttributes.id.toString =>
-                mrIid
-            }
+      private def orHalt[A](msg: String)(opt: Option[A]): F[A] = opt.fold[F[A]](halt(msg))(_.pure[F])
 
-          mergeRequestIdString
-            .flatMap { mrIId =>
-              mrIId.toLongOption.toOptionT[F].map(io.pg.gitlab.webhook.MergeRequest(_))
-            }
+      private def unhalt[A]: F[A] => F[Either[String, A]] =
+        _.map(_.asRight[String]).recover {
+          case Halt(msg) => msg.asLeft[A]
         }
 
-        pipeline.mergeRequest.toOptionT[F].orElse(mergeRequestByHeadPipeline)
+      private def decodeMergeRequest(pipeline: WebhookEvent.Pipeline): F[io.pg.gitlab.webhook.MergeRequest] = {
+        val project = pipeline.project
+
+        val mergeRequestByHeadPipeline: F[io.pg.gitlab.webhook.MergeRequest] = {
+          val PipelineId = pipeline.objectAttributes.id.toString
+
+          val query = Gitlab[F]
+            .mergeRequests(
+              projectPath = project.pathWithNamespace,
+              sourceBranches = NonEmptyList.of(pipeline.objectAttributes.ref)
+            )(
+              MergeRequest.iid ~ MergeRequest.headPipeline(Pipeline.id)
+            )
+
+          query
+            .flatMap(_.headOption.pipe(orHalt("No open MRs found for branch")))
+            .flatTap {
+              case (mrIid, Some(s"gid://gitlab/Ci::Pipeline/$PipelineId")) => Applicative[F].unit
+              case (_, None)                                               => halt[Unit]("MR didn't have a head pipeline")
+              case _                                                       => halt[Unit]("Head pipeline didn't match event's pipeline ID")
+            }
+            .map { case (mrIid, _) => mrIid }
+            .flatMap(_.toLongOption.pipe(orHalt("MR id wasn't a Long")))
+            .map(io.pg.gitlab.webhook.MergeRequest(_))
+        }
+        pipeline.mergeRequest.pipe(orHalt("Webhook event is missing MR information")).handleErrorWith {
+          case Halt(_) => mergeRequestByHeadPipeline
+        }
       }
 
       private def findMergeRequestInfo(iid: Long, project: Project): F[(String, Option[String])] =
@@ -73,7 +87,7 @@ object StateResolver {
             val project = p.project
 
             decodeMergeRequest(p)
-              .semiflatMap(mr => findMergeRequestInfo(mr.iid, project).tupleRight(mr))
+              .flatMap(mr => findMergeRequestInfo(mr.iid, project).tupleRight(mr))
               .map {
                 case ((email, description), mr) =>
                   MergeRequestState(
@@ -84,8 +98,12 @@ object StateResolver {
                     status = p.objectAttributes.status
                   )
               }
-              .value
-              .flatTap(state => Logger[F].info("Resolved MR state", Map("state" -> state.toString)))
+              .pipe(unhalt)
+              .flatTap {
+                case Left(reason) => Logger[F].debug("Couldn't build MR state", Map("reason" -> reason))
+                case Right(state) => Logger[F].info("Resolved MR state", Map("state" -> state.toString))
+              }
+              .map(_.toOption)
 
           case e                        => Logger[F].info("Ignoring event", Map("event" -> e.toString())).as(none)
         }
