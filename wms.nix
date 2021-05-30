fc1cf05d19aa787ed1656561b0bbf31aaf4577c0
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-30 23:00:49 +0200

    fix

--- wms.nix
+++ wms.nix
@@ -1,16 +1,15 @@
 let
-  pg = import (
+  pg = import ./pitgull.nix /* (
     builtins.fetchurl {
       url = "https://raw.githubusercontent.com/polyvariant/pitgull/1faf7a7c505e289895b16a481a4c156707f4c152/pitgull.nix";
       sha256 = "01ywdqqiy53mciz8m3y8ap56797dfy6v9bfrad54awprbs1qlx5j";
     }
-  );
+  ) */;
   semver = level: pg.description.matches ".*labels:.*semver-${level}.*";
   patchUpdate = (semver "patch");
   wmsLibraryMinorUpdate = pg.allOf [
     (semver "minor")
-    pg.description.matches
-    ".*((com\.ocado\.ospnow\.wms)|(com\.ocado\.gm\.wms)).*"
+    (pg.description.matches ".*((com\.ocado\.ospnow\.wms)|(com\.ocado\.gm\.wms)).*")
   ];
 in
 pg.allOf [
